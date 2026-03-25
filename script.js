f5d4ba2e0b51f65bddd59bcb472bfb278de6a768
miSkiddlz <dennismal.martin@gmail.com>
2026-03-25 17:52:09 +0100

    Refactor slider input event listener for clarity

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 console.log("Website läuft");
 
 const slider = document.getElementById('sliderRange');
-if(slider){
-  slider.addEventListener('input', function(){
-    document.querySelector('.after').style.clipPath = `inset(0 ${100-this.value}% 0 0)`;
+
+if (slider) {
+  slider.addEventListener('input', function () {
+    const after = document.querySelector('.after');
+    after.style.clipPath = `inset(0 ${100 - this.value}% 0 0)`;
   });
 }
