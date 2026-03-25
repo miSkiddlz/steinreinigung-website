7a89404ca31c6417bc84208bd7361c061cefc50d
miSkiddlz <dennismal.martin@gmail.com>
2026-03-25 16:54:31 +0100

    Add slider functionality to adjust clipping path

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Website läuft");
+
+const slider = document.getElementById('sliderRange');
+if(slider){
+  slider.addEventListener('input', function(){
+    document.querySelector('.after').style.clipPath = `inset(0 ${100-this.value}% 0 0)`;
+  });
+}
